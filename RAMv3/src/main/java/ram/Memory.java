381d3575b69fbe54eeaec836c316bba87cec7028
gubwan <iwangubler89@gmail.com>
2023-05-12 15:39:00 +0200

    working on read from file

---
+++ RAMv3/src/main/java/ram/Memory.java
@@ -0,0 +1,25 @@
+package ram;
+
+public class Memory {
+
+	private int[] memory;
+
+	public Memory(int size) {
+		memory = new int[size];
+	}
+
+	public void write(int address, int value) {
+		memory[address] = value;
+	}
+
+	public int read(int address) {
+		return memory[address];
+	}
+
+	public void print() {
+		for (int i = 0; i < memory.length; i++) {
+			System.out.println("Address " + i + ": " + memory[i]);
+		}
+	}
+
+}
