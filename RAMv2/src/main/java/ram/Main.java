c7926485c3ac646ad97006b1f71b641f831af526
gubwan <iwangubler89@gmail.com>
2023-05-12 14:15:11 +0200

    complete program class

--- RAMv2/src/main/java/ram/Main.java
+++ RAMv2/src/main/java/ram/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String[] cmds = { "LDA 1", "ADD 1" };
+		String[] cmds = { "LDA 1", "ADD 1", "ADD 1" };
+		String[] mem = { "1", "1", "1" };
 
-		Program ram = new Program(cmds);
+		Program ram = new Program(cmds, mem);
 
 	}
 
--- RAMv2/src/main/java/ram/Program.java
+++ RAMv2/src/main/java/ram/Program.java
@@ -2,19 +2,25 @@ package ram;
 
 public class Program {
 
-	public Program(String[] instructions) {
+	public Program(String[] instructions, String[] mem) {
 
-		Memory memory = new Memory(instructions.length);
+		Memory memory = new Memory(512);
 		Accumulator accumulator = new Accumulator();
 		ControllerUnit2 controllerUnit = new ControllerUnit2(memory, accumulator);
 
+		// Process the Memory
+		for (int i = 0; i < mem.length; i++) {
+			memory.write(i, Integer.parseInt(mem[i]));
+		}
+		// Process the Memory DONE
+
+		// Process the Array of Instructions START
 		for (int i = 0; i < (instructions.length); i++) {
 			String[] cmd = instructions[i].split("\\s+"); // split command into parts using whitespace as delimiter
 			controllerUnit.run(cmd[0], Integer.parseInt(cmd[1]));
 		}
-		
-		memory.print();
-		
+		// Process the Array of Instructions DONE
+
 		System.out.println("Result: " + accumulator.get());
 
 		System.out.println("ProgrammCounter: " + controllerUnit.getProgramCounter());
